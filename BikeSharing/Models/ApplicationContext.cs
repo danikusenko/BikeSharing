516f91d33535f6a4b134fc90ad45908633acde89
Daniil <danilausa99@gmail.com>
2020-05-08 20:13:05 +0300

    Lab4

--- BikeSharing/Models/ApplicationContext.cs
+++ BikeSharing/Models/ApplicationContext.cs
@@ -29,16 +29,9 @@ namespace BikeSharing.Models
             using (MySqlConnection conn = GetConnection())
             {
                 conn.Open();
-                MySqlCommand cmd = new MySqlCommand("select (case " +
-                    "when(exists(select * from clients where email = (@email) and password = " +
-                    "md5(@password) and status = @not_blocked)) then 1 " +
-                    "when(exists(select * from clients where email = (@email) " +
-                    "and password = md5(@password) and status = @blocked)) then 2 " +
-                    "else 0 end);", conn);
+                MySqlCommand cmd = new MySqlCommand("call login_client(@email, @password);", conn);
                 cmd.Parameters.AddWithValue("@email", model.Email);
-                cmd.Parameters.AddWithValue("@password", model.Password);
-                cmd.Parameters.AddWithValue("@not_blocked", "Не заблокирован");
-                cmd.Parameters.AddWithValue("@blocked", "Заблокирован");
+                cmd.Parameters.AddWithValue("@password", model.Password);                
                 result = Convert.ToInt32(cmd.ExecuteScalar());
             }
             return result;
@@ -48,41 +41,11 @@ namespace BikeSharing.Models
         {            
             using (MySqlConnection conn = GetConnection())
             {
-                conn.Open();
-                string email = "";
-                int id_client = 0;                
-                MySqlCommand cmd = new MySqlCommand("select email, id_client from clients_backup where id = (@id);", conn);
-                cmd.Parameters.AddWithValue("@id", id);
-                using (var reader = cmd.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        email = reader["email"].ToString();
-                        id_client = Convert.ToInt32(reader["id_client"]);                        
-                    }                    
-                }
-                cmd.CommandText = "select email from clients;";
-                using (var reader = cmd.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        if(email == reader["email"].ToString())                        
-                            DeleteUser(GetIdByEmail(email));
-                    }
-                }                
+                conn.Open();                
                 using (var transaction = conn.BeginTransaction())
                 {
-                    var insertCommand = conn.CreateCommand();                    
-                    insertCommand.CommandText = "insert into clients(id, id_address, " +
-                        "id_name1, id_name2, id_name3, money, email, password, phonenumber, " +
-                        "role, id_blocking,status) select id_client, id_address, id_name1, " +
-                        "id_name2, id_name3, money, email, password, phonenumber, role, " +
-                        "id_blocking, status from clients_backup where clients_backup.id = (@id);";
-                    insertCommand.CommandText += "insert into blocking(id, permanently, beginningdate, " +
-                            "expirationdate, id_client) select id_blocking, permanently, beginningdate, " +
-                            "expirationdate, id_client from blocking_backup where id_client in " +
-                            "(select id_client from clients_backup where id = (@id) and id_blocking is not null);";
-                    insertCommand.CommandText += "delete from clients_backup where id = (@id);";           
+                    var insertCommand = conn.CreateCommand();                   
+                    insertCommand.CommandText = "call recovery_client(@id);";
                     insertCommand.Parameters.AddWithValue("@id", id);
                     insertCommand.ExecuteNonQuery();
                     transaction.Commit();
@@ -109,37 +72,9 @@ namespace BikeSharing.Models
                 }
                 using (var transaction = conn.BeginTransaction())
                 {
-                    var insertCommand = conn.CreateCommand();
-                    insertCommand.CommandText = "insert into name1(lastname) select @lastname from " +
-                                                "(select @lastname) as _surname where not exists" +
-                                                "(select lastname from name1 where lastname = (@lastname)); " +
-                                                "insert into name2(firstname) select @firstname from " +
-                                                "(select @firstname) as _name  where not exists" +
-                                                "(select firstname from name2 where firstname = (@firstname)); " +
-                                                "insert into name3(patronymic) select @patronymic from " +
-                                                "(select @patronymic) as _patronymic  where not exists" +
-                                                "(select patronymic from name3 where patronymic = (@patronymic)); " +
-                                                "insert into country(name) select @country from (select @country) " +
-                                                "as _country where not exists" +
-                                                "(select name from country where name = (@country)); " +
-                                                "insert into city(name) select(@city) from (select @city) " +
-                                                "as _city where not exists" +
-                                                "(select name from city where name = (@city)); " +
-                                                "insert ignore into address(id_city,id_country)" +
-                                                "select _city.id, _country.id  from city as _city, country as _country " +
-                                                "where  _city.name = (@city) and " +
-                                                "_country.name = (@country); ";
-                    insertCommand.CommandText += "insert into clients(role, email, password, phonenumber, status, id_name1, id_name2, id_name3, id_address)" +
-                                            "select @user, @email, md5(@password), @phone_number, @status, name1.id, name2.id, " +
-                                            "name3.id, address.id " +
-                                            "from name1, name2, name3, address " +
-                                            "where name1.lastname = (@lastname) and " +
-                                            "name2.firstname = (@firstname) and " +
-                                            "name3.patronymic = (@patronymic) " +
-                                            "and address.id_city in (select city.id from city where name = (@city)) and " +
-                                            "address.id_country in (select country.id from country where name = (@country)) limit 1; ";
-                    insertCommand.Parameters.AddWithValue("@user", "Пользователь");
-                    insertCommand.Parameters.AddWithValue("@status", "Не заблокирован");
+                    var insertCommand = conn.CreateCommand();                    
+                    insertCommand.CommandText = "call register_client(@lastname, @firstname, @patronymic," +
+                        "@country, @city, @email, @password, @phone_number);";                    
                     insertCommand.Parameters.AddWithValue("@email", model.Email);
                     insertCommand.Parameters.AddWithValue("@password", model.Password);
                     insertCommand.Parameters.AddWithValue("@lastname", model.Surname);
@@ -178,32 +113,18 @@ namespace BikeSharing.Models
             return client;
         }
 
-        public List<Client> GetAllClients(SearchClientViewModel model, string table)
+        public List<Client> GetAllClients(SearchClientViewModel model, string table = "clients")
         {
-            List<Client> clients = new List<Client>();
-            string id_clients = "";
-            if (table == "clients_backup")
-                id_clients = " id_client,";
+            List<Client> clients = new List<Client>();            
             using (MySqlConnection conn = GetConnection())
             {
-                conn.Open();
-                MySqlCommand cmd = new MySqlCommand("select " + table + ".id," + id_clients +
-                     "id_blocking, money, email, phonenumber, role, status, " +
-                     "name1.lastname as surname, name2.firstname as name, " +
-                     "name3.patronymic as patronymic, country.name as country, city.name as city " +
-                     "from " + table + " join name1 on name1.id = " + table + ".id_name1 " +
-                     "join name2 on name2.id = " + table + ".id_name2 " +
-                     "join name3 on name3.id = " + table + ".id_name3 " +
-                     "join address on address.id = " + table + ".id_address " +
-                     "join country on country.id = address.id_country " +
-                     "join city on city.id = address.id_city where email like (lower(@email)) and " +
-                     "phonenumber like (@phonenumber) and id_name2 in " +
-                    "(select id from name2 where firstname like (lower(@name))) and id_name1 in " +
-                    "(select id from name1 where lastname like (lower(@surname))) and id_name3 in " +
-                    "(select id from name3 where patronymic like (lower(@patronymic))) and " +
-                    "id_address in (select id from address where id_city in (select id from city " +
-                    "where name like (lower(@city))) and id_country in (select id from country where " +
-                    "name like (lower(@country))));", conn);
+                conn.Open();                
+                string sqlQuery = "call search_clients(@email, @phonenumber, @name, @surname,  " +
+                    "@patronymic, @country, @city);";
+                if (table == "clients_backup")
+                    sqlQuery = "call search_copies(@email, @phonenumber, @name, @surname,  @patronymic, " +
+                        "@country, @city);";
+                MySqlCommand cmd = new MySqlCommand(sqlQuery, conn);                
                 cmd.Parameters.AddWithValue("@email", "%" + model.EmailSearch.ToLower() + "%");
                 cmd.Parameters.AddWithValue("@city", "%" + model.CitySearch.ToLower() + "%");
                 cmd.Parameters.AddWithValue("@country", "%" + model.CountrySearch.ToLower() + "%");
@@ -254,7 +175,7 @@ namespace BikeSharing.Models
             return clients;
         }
 
-        public void DeleteUser(string id, string table = "clients")
+        public void DeleteUser(string id)
         {
             using (MySqlConnection conn = GetConnection())
             {
@@ -262,8 +183,7 @@ namespace BikeSharing.Models
                 using (var transaction = conn.BeginTransaction())
                 {
                     var deleteCommand = conn.CreateCommand();
-                    deleteCommand.CommandText = "delete from clients where id = (@id);"/* +
-                        "delete from blocking where id_client = (@id);"*/;
+                    deleteCommand.CommandText = "call delete_client(@id);";                    
                     deleteCommand.Parameters.AddWithValue("@id", id);
                     deleteCommand.ExecuteNonQuery();
                     transaction.Commit();
@@ -272,8 +192,7 @@ namespace BikeSharing.Models
         }
 
         public void BlockUser(BlockingViewModel model)
-        {
-            int newId;
+        {            
             using (MySqlConnection conn = GetConnection())
             {
                 conn.Open();
@@ -289,19 +208,12 @@ namespace BikeSharing.Models
                 using (var transaction = conn.BeginTransaction())
                 {
                     var insertCommand = conn.CreateCommand();
-                    insertCommand.CommandText = "insert into blocking(id_client, permanently," +
-                        "beginningdate, expirationdate) values (@id_client, @permanently, " +
-                        "@beginningdate, @expirationdate); ";
+                    insertCommand.CommandText = "call block_client(@id_client, @permanently, " +
+                        "@beginningdate, @expirationdate)";
                     insertCommand.Parameters.AddWithValue("@id_client", model.Id);
                     insertCommand.Parameters.AddWithValue("@permanently", model.Permanently);
                     insertCommand.Parameters.AddWithValue("@beginningdate", model.BeginningDate);
-                    insertCommand.Parameters.AddWithValue("@expirationdate", model.ExpirationDate);
-                    insertCommand.CommandText += "select last_insert_id();";
-                    newId = Convert.ToInt32(insertCommand.ExecuteScalar());
-                    insertCommand.CommandText = "update clients set id_blocking = (@newId), status = (@block) " +
-                        "where id = (@id_client);";
-                    insertCommand.Parameters.AddWithValue("@newId", newId);
-                    insertCommand.Parameters.AddWithValue("@block", "Заблокирован");
+                    insertCommand.Parameters.AddWithValue("@expirationdate", model.ExpirationDate);                    
                     insertCommand.ExecuteScalar();
                     transaction.Commit();
                 }
@@ -316,10 +228,8 @@ namespace BikeSharing.Models
                 using (var transaction = conn.BeginTransaction())
                 {
                     var insertCommand = conn.CreateCommand();
-                    insertCommand.CommandText = "delete from blocking where id_client = (@id);" +
-                        "update clients set id_blocking = NULL, status = (@unblock) where id = (@id);";
-                    insertCommand.Parameters.AddWithValue("@id", id);
-                    insertCommand.Parameters.AddWithValue("@unblock", "Не заблокирован");
+                    insertCommand.CommandText = "call unblock_client(@id);";
+                    insertCommand.Parameters.AddWithValue("@id", id);                    
                     insertCommand.ExecuteScalar();
                     transaction.Commit();
                 }
@@ -334,10 +244,9 @@ namespace BikeSharing.Models
                 using (var transaction = conn.BeginTransaction())
                 {
                     var updateCommand = conn.CreateCommand();
-                    updateCommand.CommandText = "update clients set role = (@role) " +
-                        "where id = (@id);";
+                    updateCommand.CommandText = "call change_client_role(@id, @role)";
                     updateCommand.Parameters.AddWithValue("@id", id);
-                    updateCommand.Parameters.AddWithValue("@role", role);
+                    updateCommand.Parameters.AddWithValue("@role", role);                    
                     updateCommand.ExecuteScalar();
                     transaction.Commit();
                 }
@@ -383,7 +292,7 @@ namespace BikeSharing.Models
                     {
                         client = new Client()
                         {
-                            Id = Convert.ToInt32(reader["id"]),
+                            Id = table == "clients_backup" ? Convert.ToInt32(reader["id_client"]) : Convert.ToInt32(reader["id"]),
                             FirstName = new Name2
                             {
                                 FirstName = reader["name"].ToString()
